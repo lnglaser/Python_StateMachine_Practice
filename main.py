dace8d456c4f877115229cde46019989c9a54f45
Leon Glaser <leon.glaser@gmail.com>
2024-02-07 12:53:38 -0500

    practicing use of state machines for Mille Bornes Solitaire V2

---
+++ main.py
@@ -0,0 +1,19 @@
+from statemachine import StateMachine, State
+
+
+class LightBulb(StateMachine):
+
+    # creating states
+    offState = State("off", initial=True)
+    onState = State("on")
+
+    # transitions of the state
+    switchOn = offState.to(onState)
+    switchOff = onState.to(offState)
+
+
+bulb = LightBulb()
+print(bulb.current_state)
+
+a = [s.id for s in bulb.states]
+print(a)
